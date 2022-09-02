023941f840d6a4d78830c6cc78e9440def5629af
Ardianto <ardianto8306@gmail.com>
2022-09-02 09:25:16 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AdminProductController;
 use Illuminate\Support\Facades\Route;
@@ -18,7 +17,7 @@ Route::get('/about', [HomeController::class, 'about']);
 Route::get('/contact', [HomeController::class, 'contact']);
 
 //Routing Admin
-Route::get('/admin-dashboard' . AdminController::class, 'index');
+Route::get('/admin-dashboard', [AdminController::class, 'index']);
 Route::resource('admin-product', AdminProductController::class);
 
 // Auth Route
