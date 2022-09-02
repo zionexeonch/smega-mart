1300652aa88308309d5f7d259591490747770ea7
Ardianto <ardianto8306@gmail.com>
2022-09-02 15:36:01 +0700

    home

--- resources/views/layouts/layout-main.blade.php
+++ resources/views/layouts/layout-main.blade.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{ $title }} | SMEGAMART</title>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;400;600;700&family=Poppins:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title }} | SMEGAMART</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;400;600;700&family=Poppins:wght@200;300;400;500;600;700;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+
 <body>
     <header class="bg-white">
         @include('layouts.partials.header.header-top')
@@ -19,16 +23,22 @@
     </header>
 
     <div class="py-3 lg:py-8 px-2">
-      @yield('content')
+        @yield('content')
+    </div>
+
+    {{-- partner --}}
+    <div class="bg-gray-300">
+        @include('pages.partner')
     </div>
 
     <footer class="bg-white">
-      @include('layouts.partials.footer.footer-mid')
-      @include('layouts.partials.footer.footer-bottom')
+        @include('layouts.partials.footer.footer-mid')
+        @include('layouts.partials.footer.footer-bottom')
     </footer>
 
-  <script src="{{ asset('js/app.js') }}"></script>
-  <script src="{{ asset('js/flowbite.js') }}"></script>
-  <script defer src="{{ asset('assets/fontawesome/js/all.min.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/flowbite.js') }}"></script>
+    <script defer src="{{ asset('assets/fontawesome/js/all.min.js') }}"></script>
 </body>
+
 </html>
---
+++ resources/views/pages/partner.blade.php
@@ -0,0 +1,11 @@
+<div class="bg-gray-300">
+    <h2 class="text-center text-2xl">Sponsored:</h2>
+    <div class="grid grid-cols-2 justify-items-center">
+        <div class="mx-auto my-5 mr-5">
+            <img src="/assets/img/alfamart.png" alt="" class="w-20 h-auto md:w-40 md:h-auto">
+        </div>
+        <div class="mx-auto my-5 ml-5">
+            <img src="/assets/img/smk.png" alt="" class="w-20 h-auto md:w-40 md:h-auto">
+        </div>
+    </div>
+</div>
