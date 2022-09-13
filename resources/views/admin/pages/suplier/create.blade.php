716096d07295e13df8cbe0b7353947e619472671
Ardiputra614 <ardianto8306@gmail.com>
2022-09-13 15:04:04 +0700

    suplier

--- resources/views/admin/pages/suplier/create.blade.php
+++ resources/views/admin/pages/suplier/create.blade.php
@@ -1,3 +1,19 @@
 @extends('admin.layouts.template')
 @section('content')
+    <form action="" method="POST" class="mb-6">
+        <div class="grid gap-6 mb-6 md:grid-cols-2">
+            <div>
+                <label for="name" class="block mb-2 text-sm font-medium">Nama Suplier</label>
+                <input type="text" id="name"
+                    class="shadow-md border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Nama Suplier" required="" name="name">
+            </div>
+        </div>
+        <div class="mb-6">
+            <button type="submit"
+                class="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Simpan</button>
+            <a href="/dashboard/products" type="button"
+                class="mt-3 text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Batal</a>
+        </div>
+    </form>
 @endsection
