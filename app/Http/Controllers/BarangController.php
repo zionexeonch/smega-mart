02e1153972e49bf4ca18b19fbd448207ca706875
Ardiputra614 <ardianto8306@gmail.com>
2022-09-15 12:21:32 +0700

    Barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -2,17 +2,11 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD:app/Http/Controllers/BarangController.php
-use App\Http\Requests\StoreProductRequest;
-use App\Http\Requests\UpdateProductRequest;
-use App\Models\Barang;
-=======
-use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
->>>>>>> 9dbb600 (Admin):app/Http/Controllers/ProductController.php
+use App\Models\Barang;
 
-class ProductController extends Controller
+class BarangController extends Controller
 {
   /**
    * Display a listing of the resource.
@@ -47,19 +41,16 @@ class ProductController extends Controller
    */
   public function store(Request $request)
   {
-    Product::create([
-      'category_id' => $request->category_id,
-      'name' => $request->name,
-      'slug' => Str::slug($request->slug),
-      'unit_id' => $request->unit_id,
-      'buy_price' => $request->buy_price,
-      'sold_price' => $request->sold_price,
-      'variant' => $request->variant,
-      'store_stock' => $request->store_stock,
-      'storage_stock' => $request->storage_stock,
-      'weight' => $request->weight,
-      'description' => $request->description,
-      'image_urls' => $request->image_urls,
+    Barang::create([
+      'barcode' => $request->barcode,
+      'namaBarang' => $request->namaBarang,
+      'slug' => $request->slug,
+      'kdKategori' => $request->kdKategori,
+      'hrgBeli' => $request->hrgBeli,
+      'hrgJual' => $request->hrgJual,
+      'stok' => $request->stok,
+      'deskripsi' => $request->deskripsi,
+      'img_urls' => $request->img_urls,
     ]);
     return redirect()->to('product')->with('success', 'Produk berhasil ditambah!');
   }
@@ -70,15 +61,9 @@ class ProductController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-<<<<<<< HEAD:app/Http/Controllers/BarangController.php
-  public function show(Barang $product)
-  {
-    $product = Barang::find($product->id);
-=======
   public function show($id)
   {
-    $product =  Product::findOrFail($id);
->>>>>>> 9dbb600 (Admin):app/Http/Controllers/ProductController.php
+    $product =  Barang::findOrFail($id);
     return view('admin.pages.product.detail', [
       'title' => 'Detail',
       'product' => $product,
@@ -91,15 +76,9 @@ class ProductController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-<<<<<<< HEAD:app/Http/Controllers/BarangController.php
-  public function edit(Barang $product)
-  {
-    $product = Barang::find($product->id);
-=======
   public function edit($id)
   {
-    $product = Product::findOrFail($id);
->>>>>>> 9dbb600 (Admin):app/Http/Controllers/ProductController.php
+    $product = Barang::findOrFail($id);
     return view('admin.pages.product.edit', [
       'title' => 'Edit Produk',
       'product' => $product,
@@ -109,19 +88,11 @@ class ProductController extends Controller
   /**
    * Update the specified resource in storage.
    *
-<<<<<<< HEAD:app/Http/Controllers/BarangController.php
-   * @param  \App\Http\Requests\UpdateProductRequest  $request
-   * @param  \App\Models\Barang  $product
-   * @return \Illuminate\Http\Response
-   */
-  public function update(UpdateProductRequest $request, Barang $product)
-=======
    * @param  \Illuminate\Http\Request  $request
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
   public function update(Request $request, $id)
->>>>>>> 9dbb600 (Admin):app/Http/Controllers/ProductController.php
   {
     //
   }
@@ -132,11 +103,7 @@ class ProductController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-<<<<<<< HEAD:app/Http/Controllers/BarangController.php
-  public function destroy(Barang $product)
-=======
   public function destroy($id)
->>>>>>> 9dbb600 (Admin):app/Http/Controllers/ProductController.php
   {
     //
   }
